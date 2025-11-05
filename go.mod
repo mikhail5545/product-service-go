4f57e0187c91dc510c4a20a4978ca38c0139b759
mikail5545 <mikailkulik07@gmail.com>
2025-11-05 12:34:51 +0300

    New routes + structs -> interfaces

--- go.mod
+++ go.mod
@@ -6,30 +6,35 @@ toolchain go1.24.9
 
 require (
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
-	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/mikhail5545/proto-go v0.1.18
+	github.com/stretchr/testify v1.10.0
 	go.uber.org/mock v0.6.0
 	google.golang.org/grpc v1.76.0
 	google.golang.org/protobuf v1.36.10
 	gorm.io/driver/postgres v1.6.0
+	gorm.io/driver/sqlite v1.6.0
 	gorm.io/gorm v1.31.0
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
@@ -39,5 +44,5 @@ require (
 	golang.org/x/text v0.27.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250804133106-a7a43d27e69b // indirect
-	gorm.io/driver/sqlite v1.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -22,16 +22,17 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	admincourse "github.com/mikhail5545/product-service-go/internal/handlers/admin/course"
 	admincp "github.com/mikhail5545/product-service-go/internal/handlers/admin/course_part"
-	adminproduct "github.com/mikhail5545/product-service-go/internal/handlers/admin/product"
+	adminphysicalgood "github.com/mikhail5545/product-service-go/internal/handlers/admin/physical_good"
 	adminseminar "github.com/mikhail5545/product-service-go/internal/handlers/admin/seminar"
 	admints "github.com/mikhail5545/product-service-go/internal/handlers/admin/training_session"
 	publiccourse "github.com/mikhail5545/product-service-go/internal/handlers/public/course"
 	publiccp "github.com/mikhail5545/product-service-go/internal/handlers/public/course_part"
-	publicproduct "github.com/mikhail5545/product-service-go/internal/handlers/public/product"
+	publicphysicalgood "github.com/mikhail5545/product-service-go/internal/handlers/public/physical_good"
 	publicseminar "github.com/mikhail5545/product-service-go/internal/handlers/public/seminar"
 	publicts "github.com/mikhail5545/product-service-go/internal/handlers/public/training_session"
 	"github.com/mikhail5545/product-service-go/internal/services/course"
 	coursepart "github.com/mikhail5545/product-service-go/internal/services/course_part"
+	physicalgood "github.com/mikhail5545/product-service-go/internal/services/physical_good"
 	"github.com/mikhail5545/product-service-go/internal/services/product"
 	"github.com/mikhail5545/product-service-go/internal/services/seminar"
 	trainingsession "github.com/mikhail5545/product-service-go/internal/services/training_session"
@@ -40,11 +41,12 @@ import (
 
 func Setup(
 	e *echo.Echo,
-	productService *product.Service,
-	cpService *coursepart.Service,
-	tsService *trainingsession.Service,
-	courseService *course.Service,
-	seminarService *seminar.Service,
+	productService product.Service,
+	cpService coursepart.Service,
+	tsService trainingsession.Service,
+	courseService course.Service,
+	seminarService seminar.Service,
+	phgService physicalgood.Service,
 ) {
 	e.HTTPErrorHandler = errors.HTTPErrorHandler
 
@@ -55,24 +57,19 @@ func Setup(
 	e.Use(middleware.Recover())
 
 	// --- Public handlers ---
-	productHandler := publicproduct.New(productService)
+	phgHandler := publicphysicalgood.New(phgService)
 	cpHandler := publiccp.New(cpService)
 	tsHandler := publicts.New(tsService)
 	courseHandler := publiccourse.New(courseService)
 	seminarHandler := publicseminar.New(seminarService)
 
 	// --- Admin handlers ---
-	adminProductHandler := adminproduct.New(productService)
+	adminphgHandler := adminphysicalgood.New(phgService)
 	admincpHandler := admincp.New(cpService)
 	admintsHandler := admints.New(tsService)
 	adminCourseHandler := admincourse.New(courseService)
 	adminSeminarHandler := adminseminar.New(seminarService)
 
-	products := ver.Group("/products")
-	{
-		products.GET("", productHandler.List)
-		products.GET("/:id", productHandler.Get)
-	}
 	trainingSesssions := ver.Group("/training-sessions")
 	{
 		trainingSesssions.GET("", tsHandler.List)
@@ -93,44 +90,94 @@ func Setup(
 		seminars.GET("", seminarHandler.List)
 		seminars.GET("/:id", seminarHandler.Get)
 	}
-
+	physicalGoods := ver.Group("/physical-good")
+	{
+		physicalGoods.GET("", phgHandler.List)
+		physicalGoods.GET("/:id", phgHandler.Get)
+	}
 	admin := ver.Group("/admin")
 	{
-		adminProducts := admin.Group("/products")
+		adminPhysicalGoods := admin.Group("/physical-good")
 		{
-			adminProducts.GET("", adminProductHandler.List)
-			adminProducts.GET("/:id", adminProductHandler.Get)
-			adminProducts.POST("", adminProductHandler.Create)
-			adminProducts.PATCH("/:id", adminProductHandler.Update)
-			adminProducts.DELETE("/:id", adminProductHandler.Delete)
+			adminPhysicalGoods.GET("", adminphgHandler.List)
+			adminPhysicalGoods.GET("/deleted", adminphgHandler.ListDeleted)
+			adminPhysicalGoods.GET("/unpublished", adminphgHandler.ListUnpublished)
+			adminPhysicalGoods.GET("/:id", adminphgHandler.Get)
+			adminPhysicalGoods.GET("/deleted/:id", adminphgHandler.GetWithDeleted)
+			adminPhysicalGoods.GET("/unpublished/:id", adminphgHandler.GetWithUnpublished)
+			adminPhysicalGoods.POST("", adminphgHandler.Create)
+			adminPhysicalGoods.PATCH("/:id", adminphgHandler.Update)
+			adminPhysicalGoods.POST("/publish/:id", adminphgHandler.Publish)
+			adminPhysicalGoods.POST("/unpublish/:id", adminphgHandler.Unpublish)
+			adminPhysicalGoods.POST("/restore/:id", adminphgHandler.Restore)
+			adminPhysicalGoods.DELETE("/:id", adminphgHandler.Delete)
+			adminPhysicalGoods.DELETE("/permanent/:id", adminphgHandler.DeletePermanent)
 		}
 		adminTrainingSessions := admin.Group("/training-sessions")
 		{
 			adminTrainingSessions.GET("", admintsHandler.List)
+			adminTrainingSessions.GET("/deleted", admintsHandler.ListDeleted)
+			adminTrainingSessions.GET("/unpublished", admintsHandler.ListUnpublished)
 			adminTrainingSessions.GET("/:id", admintsHandler.Get)
+			adminTrainingSessions.GET("/deleted/:id", admintsHandler.GetWithDeleted)
+			adminTrainingSessions.GET("/unpublished/:id", admintsHandler.GetWithUnpublished)
 			adminTrainingSessions.POST("", admintsHandler.Create)
 			adminTrainingSessions.PATCH("/:id", admintsHandler.Update)
+			adminTrainingSessions.POST("/publish/:id", admintsHandler.Publish)
+			adminTrainingSessions.POST("/unpublish/:id", admintsHandler.Unpublish)
+			adminTrainingSessions.POST("/restore/:id", admintsHandler.Restore)
 			adminTrainingSessions.DELETE("/:id", admintsHandler.Delete)
+			adminTrainingSessions.DELETE("/permanent/:id", admintsHandler.DeletePermanent)
 		}
 		adminCourses := admin.Group("/courses")
 		{
 			adminCourses.GET("", adminCourseHandler.List)
+			adminCourses.GET("/deleted", adminCourseHandler.ListDeleted)
+			adminCourses.GET("/unpublished", adminCourseHandler.ListUnpublished)
 			adminCourses.GET("/:id", adminCourseHandler.Get)
+			adminCourses.GET("/deleted/:id", adminCourseHandler.GetWithDeleted)
+			adminCourses.GET("/unpublished/:id", adminCourseHandler.GetWithUnpublished)
 			adminCourses.POST("", adminCourseHandler.Create)
 			adminCourses.PATCH("/:id", adminCourseHandler.Update)
+			adminCourses.POST("/publish/:id", adminCourseHandler.Publish)
+			adminCourses.POST("/unpublish/:id", adminCourseHandler.Unpublish)
+			adminCourses.DELETE("/:id", adminCourseHandler.Delete)
+			adminCourses.DELETE("/permanent/:id", adminCourseHandler.DeletePermanent)
+			adminCourses.POST("restore/:id", adminCourseHandler.Restore)
+			// --- Course parts assigned to the course ---
+			adminCourses.GET("/:cid/parts/", admincpHandler.List)
+			adminCourses.GET("/:cid/parts/deleted", admincpHandler.ListDeleted)
+			adminCourses.GET("/:cid/parts/unpublished", admincpHandler.ListUnpublished)
+			adminCourses.POST("/:cid/parts", admincpHandler.Create)
 		}
 		adminCourseParts := admin.Group("/course-parts")
 		{
-			adminCourseParts.GET("", admincpHandler.List)
 			adminCourseParts.GET("/:id", admincpHandler.Get)
+			adminCourseParts.GET("/deleted/:id", admincpHandler.GetWithDeleted)
+			adminCourseParts.GET("/unpublished/:id", admincpHandler.GetWithUnpublished)
+			adminCourseParts.POST("/publish/:id", admincpHandler.Publish)
+			adminCourseParts.POST("/unpublish/:id", admincpHandler.Unpublish)
+			adminCourseParts.POST("/video/:id", admincpHandler.AddVideo)
+			adminCourseParts.POST("/restore/:id", admincpHandler.Restore)
+			adminCourseParts.PATCH("/:id", admincpHandler.Update)
+			adminCourseParts.DELETE("/:id", admincpHandler.Delete)
+			adminCourseParts.DELETE("/permanent/:id", admincpHandler.DeletePermanent)
 		}
 		adminSeminars := admin.Group("/seminars")
 		{
 			adminSeminars.GET("", adminSeminarHandler.List)
+			adminSeminars.GET("/deleted", adminSeminarHandler.ListDeleted)
+			adminSeminars.GET("/unpublished", adminSeminarHandler.ListUnpublished)
 			adminSeminars.GET("/:id", adminSeminarHandler.Get)
+			adminSeminars.GET("/deleted/:id", adminSeminarHandler.GetWithDeleted)
+			adminSeminars.GET("/unpublished/:id", adminSeminarHandler.GetWithUnpublished)
 			adminSeminars.POST("", adminSeminarHandler.Create)
 			adminSeminars.PATCH("/:id", adminSeminarHandler.Update)
+			adminSeminars.POST("/publish/:id", adminSeminarHandler.Publish)
+			adminSeminars.POST("/unpublish/:id", adminSeminarHandler.Unpublish)
+			adminSeminars.POST("/restore/:id", adminSeminarHandler.Restore)
 			adminSeminars.DELETE("/:id", adminSeminarHandler.Delete)
+			adminSeminars.DELETE("/permanent/:id", adminSeminarHandler.DeletePermanent)
 		}
 	}
 }
