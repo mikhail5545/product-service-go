0d10ab2e6ac1f1cfff189c39d48993cb06de69ea
mikail5545 <mikailkulik07@gmail.com>
2025-11-11 16:34:43 +0300

    Image client/server gRPC implementation

--- internal/server/image/server.go
+++ internal/server/image/server.go
@@ -29,13 +29,31 @@ import (
 	imageservice "github.com/mikhail5545/product-service-go/internal/services/image"
 	"github.com/mikhail5545/product-service-go/internal/util/errors"
 	imagepb "github.com/mikhail5545/proto-go/proto/product_service/image/v0"
+	"google.golang.org/grpc"
 )
 
+// Server implements [imagepb.UnimplementedImageServiceServer] and provides
+// various operations for images. It acts as an adapter between
+// gRPC server and the business service-layer logic from [imageservice.Service].
 type Server struct {
 	imagepb.UnimplementedImageServiceServer
 	service imageservice.Service
 }
 
+// New creates a new Server instance.
+func New(svc imageservice.Service) *Server {
+	return &Server{service: svc}
+}
+
+// Register registers the image server with a gRPC server instance.
+func Register(s *grpc.Server, svc imageservice.Service) {
+	imagepb.RegisterImageServiceServer(s, New(svc))
+}
+
+// Add adds an image for the owner depending on specified ownerType.
+//
+// Returns an `InvalidArgument` gRPC error if the request payload or ownerType is invalid.
+// Returns a `NotFound` gRPC error if the image not found on the owner or the owner is not found.
 func (s *Server) Add(ctx context.Context, req *imagepb.AddRequest) (*imagepb.AddResponse, error) {
 	addReq := &imagemodel.AddRequest{
 		PublicID:       req.GetPublicId(),
@@ -51,6 +69,10 @@ func (s *Server) Add(ctx context.Context, req *imagepb.AddRequest) (*imagepb.Add
 	return &imagepb.AddResponse{MediaServiceId: req.GetMediaServiceId(), OwnerId: req.GetOwnerId()}, nil
 }
 
+// Delete deletes an image from the owner depending on specified ownerType.
+//
+// Returns an `InvalidArgument` gRPC error if the request payload or ownerType is invalid.
+// Returns a `NotFound` gRPC error if the image not found on the owner or the owner is not found.
 func (s *Server) Delete(ctx context.Context, req *imagepb.DeleteRequest) (*imagepb.DeleteResponse, error) {
 	deleteReq := &imagemodel.DeleteRequest{
 		OwnerID:        req.GetOwnerId(),
@@ -63,6 +85,11 @@ func (s *Server) Delete(ctx context.Context, req *imagepb.DeleteRequest) (*image
 	return &imagepb.DeleteResponse{MediaServiceId: req.GetMediaServiceId(), OwnerId: req.GetOwnerId()}, nil
 }
 
+// AddBatch adds an image for the batch of owners depending on specified ownerType.
+//
+// Returns the number of affected owners.
+// Returns an `InvalidArgument` gRPC error if the request payload or ownerType is invalid.
+// Returns a `NotFound` gRPC error if none or the owners were found.
 func (s *Server) AddBatch(ctx context.Context, req *imagepb.AddBatchRequest) (*imagepb.AddBatchResponse, error) {
 	addReq := &imagemodel.AddBatchRequest{
 		PublicID:       req.GetPublicId(),
@@ -78,6 +105,11 @@ func (s *Server) AddBatch(ctx context.Context, req *imagepb.AddBatchRequest) (*i
 	return &imagepb.AddBatchResponse{OwnersAffected: int64(ownersAffected)}, nil
 }
 
+// DeleteBatch deletes an image from the batch of owners depending on specified ownerType.
+//
+// Returns the number of affected owners.
+// Returns an `InvalidArgument` gRPC error if the request payload or ownerType is invalid.
+// Returns a `NotFound` gRPC error if none or the owners were found or associations were not found.
 func (s *Server) DeleteBatch(ctx context.Context, req *imagepb.DeleteBatchRequest) (*imagepb.DeleteBatchResponse, error) {
 	deleteReq := &imagemodel.DeleteBatchRequst{
 		MediaServiceID: req.GetMediaServiceId(),
